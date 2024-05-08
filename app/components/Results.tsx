7f5b9370f7625f861e3031f3b9ccc5cf3e5dca98
= <saumyabshah90@gmail.com>
2024-05-08 14:13:24 +0530

    fix: ui

--- app/components/Results.tsx
+++ app/components/Results.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 const Results = ({isspam,reason}:{isspam:boolean, reason:string | undefined}) => {
-  const reasonLines = reason ? reason.split('\n').map((line, i) => <span key={i} className='text-slate-50 text-left font-semibold text-lg'>{line}</span>) : null;
+  const reasonLines = reason ? reason.split('\n').map((line, i) => <span key={i} className='text-slate-50 text-left font-semibold text-lg mb-1'>{line}</span>) : "";
 
   return (
     <div className='w-full'>
       {isspam ? (
-        <div className='flex flex-col items-center gap-6'>
-            <h1 className='text-red-500 p-2 rounded-md text-5xl font-bold'>Spam</h1>
+        <div className='flex flex-col gap-4'>
+            <h1 className='text-red-500 p-2 rounded-md text-5xl text-center font-bold'>Spam</h1>
             {reasonLines}
         </div>
         ) : (
-        <div className='flex flex-col items-center gap-6'>
-            <h1 className='text-green-500 p-2 rounded-md text-5xl font-bold'>Not Spam</h1>
+        <div className='flex flex-col gap-4'>
+            <h1 className='text-green-500 p-2 rounded-md text-5xl text-center font-bold'>Not Spam</h1>
             {reasonLines}
         </div>
         )}
